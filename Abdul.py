586b870c27ea9220120a85618f155fbf658625b1
abdulwaheed0000 <81903150+abdulwaheed0000@users.noreply.github.com>
2021-04-04 10:25:37 +0100

    Add files via upload

---
+++ Abdul.py
@@ -0,0 +1,38 @@
+print('Question 1')
+MyName='ABDULWAHEED ABDULRAFIU'
+
+MyDepartment='SCIENCE EDUCATION'
+
+
+print('My name is',MyName)
+
+print('I am from',MyDepartment)
+
+print('Question 2')
+scare='boo!'
+
+print(scare)
+
+print('Question 3')
+numb1=5
+numb2=7
+numb3=numb1 + numb2
+print(numb1,numb2,numb3)
+print('Quetion 4')
+result=12/0.5
+print(result)
+print('Question 5')
+
+Abdul=25+30
+print(Abdul)
+print('Question 6')
+Abdul=8%3
+print(Abdul)
+print('Question 7')
+result1=2 * 4
+
+result2=4 + 2
+
+result3=result1 * result2
+
+print(result3)
